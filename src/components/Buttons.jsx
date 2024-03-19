863f79524c9100ba580ee563c044cbac5441652b
maxwell.maier <maxwellmaier89@gmail.com>
2024-03-18 20:51:44 -0500

    readability changes to code

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,22 +1,4 @@
-// import React from 'react';
-// import './styling.css'; // Import styling.css
-// const Buttons = ({ filterByType, setSat, displaySats }) => {
-//   return (
-//     <div>
-//       {displaySats.map((sat, id) => (
-//         <button onClick={() => filterByType(sat)} key={id}>
-//           {sat} Orbit
-//         </button>
-//       ))}
-//       <button onClick={() => setSat(satData)}>All Orbits</button>
-//     </div>
-//   );
-// };
 
-// export default Buttons;
-
-
-// Buttons.jsx
 import React from 'react';
 import satData from './satData'; // Import satData
 

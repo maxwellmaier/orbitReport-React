cb880609c894fe90ccfa14cde9ee5cbc75ee31c2
maxwell.maier <maxwellmaier89@gmail.com>
2024-03-18 20:48:14 -0500

    all satelites displays properly

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,14 +1,38 @@
+// import React from 'react';
+// import './styling.css'; // Import styling.css
+// const Buttons = ({ filterByType, setSat, displaySats }) => {
+//   return (
+//     <div>
+//       {displaySats.map((sat, id) => (
+//         <button onClick={() => filterByType(sat)} key={id}>
+//           {sat} Orbit
+//         </button>
+//       ))}
+//       <button onClick={() => setSat(satData)}>All Orbits</button>
+//     </div>
+//   );
+// };
+
+// export default Buttons;
+
+
+// Buttons.jsx
 import React from 'react';
-import './styling.css'; // Import styling.css
+import satData from './satData'; // Import satData
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
+  const handleAllOrbitsClick = () => {
+    setSat(satData); 
+  };
+
   return (
-    <div>
+    <div className="flex-container">
       {displaySats.map((sat, id) => (
         <button onClick={() => filterByType(sat)} key={id}>
           {sat} Orbit
         </button>
       ))}
-      <button onClick={() => setSat(satData)}>All Orbits</button>
+      <button onClick={handleAllOrbitsClick}>All Orbits</button>
     </div>
   );
 };
